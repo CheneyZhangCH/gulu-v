32747232e04ee6d8009bc55d4e72a0529dbfba83
张晨辉 <cheneyzhangchenhui@gmail.com>
2021-04-12 09:58:34 +0800

    优化css

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -76,11 +76,10 @@ export default {
 aside {
     background: lightblue;
     width: 150px;
-    padding: 16px;
+    padding: 70px 16px 16px;
     position: fixed;
     top: 0;
     left: 0;
-    padding-top: 70px;
     height: 100%;
     > h2 {
         margin-bottom: 4px;
