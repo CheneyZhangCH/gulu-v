5048a30c481a0ce3e03e2a6ef7a7f58fdc20f97e
张晨辉 <cheneyzhangchenhui@gmail.com>
2021-04-24 21:20:06 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,56 +1,22 @@
 <template>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
-    <div>Button 文档</div>
+  <h1>示例1</h1>
+  <div>
+    <Button>你好</Button>
+  </div>
 </template>
 
 <script lang="ts">
+import Button from '../lib/Button.vue'
+import { ref } from 'vue'
 
-export default {}
+export default {
+  components: {
+    Button,
+  },
+  setup() {
+    return {}
+  },
+}
 </script>
 
 <style lang="scss" scoped>
--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -16,7 +16,6 @@ export default {
         return { bool }
 
     },
-
 }
 </script>
 
